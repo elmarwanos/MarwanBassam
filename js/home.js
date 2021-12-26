ed7b01f3696c7613ad97e96e1ba4d91a13be40f9
elmarwanos <marwanbassam13@gmail.com>
2021-12-27 00:20:18 +0200

    Redesign

--- js/home.js
+++ js/home.js
@@ -1,35 +1,26 @@
-function selectTitle() {
-    $(".title").addClass("selected").removeClass("blinking");
-    $(".title:hover").css("cursor","text")
-    $(".navbar").css("display", "block");
-    
-}
-
-function renewStar(e) {
-    //called by addStar when animation ends
-    $("#stars-container").append(e);
-}
-
-function addStar(amount, sclass) {
-    for (let index = 0; index < amount; index++) {
-        $("#stars-container").append(
-            $("<div>")
-                .addClass("star")
-                .addClass(sclass)
-                .css("top", `${Math.random() * window.innerHeight * 4}px`)
-                .css("left", `${Math.random() * window.innerWidth}px`)
-                .one(
-                    "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-                    function () {
-                        renewStar(this);
-                    }
-                )
-        );
-    }
+function $(e) {
+    return document.getElementById(e);
 }
 
 window.onload = function () {
+    document.getElementById("title").onclick = selectTitle;
     addStar(20, "star1");
     addStar(40, "star2");
     addStar(120, "star3");
 };
+
+function selectTitle() {
+    $("title").className = "selected";
+    $("hyperjump").className = "zoom";
+    setTimeout(() => {window.location.href = "info.html"}, 4000);
+}
+
+function addStar(amount, sclass) {
+    for (let index = 0; index < amount; index++) {
+        var tempStar = document.createElement("div");
+        tempStar.className = `star ${sclass}`;
+        tempStar.style.top = `${Math.random() * 300}%`;
+        tempStar.style.left = `${Math.random() * 100}%`;
+        $("stars-container").append(tempStar);
+    }
+}
