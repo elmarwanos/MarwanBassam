012e41560f3781f12a5d126ef114f7d972f568f4
Marwan Bassam <marwanbassam13@gmail.com>
2022-01-12 17:40:02 +0200

    Added Mario blocks to skills

--- js/info.js
+++ js/info.js
@@ -3,3 +3,15 @@ function $(e) { return document.getElementById(e); }
 window.onscroll = () => {
     $("me").className = "scroll-pos";
 }
+
+window.onload = () => {
+    const marioBlocks = document.querySelectorAll(".table-container > img");
+    for (let i = 0; i < marioBlocks.length; i++) {
+        marioBlocks[i].addEventListener("click", blockClick);
+    }
+}
+
+function blockClick(e) {
+    e.target.parentNode.classList.replace("unopened", "opened");
+    
+}
