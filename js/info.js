c108ea22f982ec70821a4562f3e773a38bf8ca26
Marwan Bassam <marwanbassam13@gmail.com>
2022-04-05 13:09:24 +0300

    Added move back animation to portrait

--- js/info.js
+++ js/info.js
@@ -1,18 +1,27 @@
 function $(e) { return document.getElementById(e); }
 
 window.onscroll = () => {
+    //Move portrait on scroll down
     $("me").className = "scroll-pos";
+
+    //Reposition portrait on scroll up
+    if(window.scrollY == 0) {
+        $("me").className = "frozen-pos";
+    }
 }
 
 window.onload = () => {
+    //Development warning
     alert("This page is still being developed so it is subject to change!");
+
+    //Set up for mario blocks
     const marioBlocks = document.querySelectorAll(".table-container > img");
     for (let i = 0; i < marioBlocks.length; i++) {
         marioBlocks[i].addEventListener("click", blockClick);
     }
 }
 
+//Click mario box to open
 function blockClick(e) {
     e.target.parentNode.classList.replace("unopened", "opened");
-    
 }
